5bfd14d961d0435600249a442e201799eeb3eca1
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2026-05-25 16:53:04 +0200

    fix: more explicit reason on assertion failures

--- rmtree/struct/metadata.py
+++ rmtree/struct/metadata.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union, Literal
 
 from rmtree.struct.content import Content, FileType
 
@@ -28,10 +28,14 @@ class Metadata:
     def get_associated_content(self) -> Optional[Content]:
         return Content.from_file(self.src, self.uuid, self.get_file_type())
 
-    def test_assertion(self, uuid_list: list[str]) -> bool:
+    def test_assertion(self, uuid_list: list[str]) -> Union[Literal[True], str]:
         # the metadata contains is type, visibleName and parent
         # - type is one of FileType valid type
         # - parent is in uuid
-        return all([p in self.raw for p in ["type", "visibleName", "parent"]]) \
-            and self.raw["parent"] in uuid_list or self.raw["parent"] in ["", "trash"] \
-            and FileType.valid_type(self.raw["type"])
+        if not all([p in self.raw for p in ["type", "visibleName", "parent"]]):
+            return "Missing attributes in .metadata file."
+        if not (self.raw["parent"] in uuid_list or self.raw["parent"] in ["", "trash"]):
+            return "Can't find parent of this file."
+        if not FileType.valid_type(self.raw["type"]):
+            return "This document has an unknown type."
+        return True
